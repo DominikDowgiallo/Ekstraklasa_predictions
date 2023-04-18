ef657608770c5ac40cd986f0ccf11a3327dfba6d
ja <abc@gmail.com>
2023-04-18 21:08:14 +0200

    Add Cleaning_data

--- scrapper.py
+++ scrapper.py
@@ -90,9 +90,6 @@ def get_matches(season):
         for i in range(0, len(all_stats_home)):
             stats_home.append(all_stats_home[i].text)
             stats_away.append(all_stats_away[i].text)
-        print(stats_home)
-        print(len(stats_home))
-        print(stats_away)
 
         # add all stats
         dict = {
@@ -108,48 +105,6 @@ def get_matches(season):
             dict[all_stats_category[i].text + '_away'] = all_stats_away[i].text
         matches.append(dict)
 
-
-        # matches.append({
-        #                 "Season" : season,
-        #                 "Home Team": stats_home[0],
-        #                 "ball_posession_home": stats_home[1],
-        #                 "goal_situations_home": stats_home[2],
-        #                 "shots_on_target_home": stats_home[3],
-        #                 "shots_no_target_home": stats_home[4],
-        #                 "blocked_shots_home": stats_home[5],
-        #                 "free_kicks_home": stats_home[6],
-        #                 "corners_home": stats_home[7],
-        #                 "offsides_home": stats_home[8],
-        #                 "auts_home": stats_home[9],
-        #                 "interventions_home": stats_home[10],
-        #                 "fauls_home": stats_home[11],
-        #                 "red_cards_home": stats_home[12],
-        #                 "yellow_cards_home": stats_home[13],
-        #                 "passes_home": stats_home[14],
-        #                 "passes_target_home": stats_home[15],
-        #                 "blocks_home": stats_home[16],
-        #                 "attacks_home": stats_home[17],
-        #                 "dang_attacks_home": stats_home[18],
-        #                 "Away Team": stats_away[0],
-        #                 "ball_posession_away": stats_away[1],
-        #                 "goal_situations_away": stats_away[2],
-        #                 "shots_on_target_away": stats_away[3],
-        #                 "shots_no_target_away": stats_away[4],
-        #                 "blocked_shots_away": stats_away[5],
-        #                 "free_kicks_away": stats_away[6],
-        #                 "corners_away": stats_away[7],
-        #                 "offsides_away": stats_away[8],
-        #                 "auts_away": stats_away[9],
-        #                 "interventions_away": stats_away[10],
-        #                 "fauls_away": stats_away[11],
-        #                 "red_cards_away": stats_away[12],
-        #                 "yellow_cards_away": stats_away[13],
-        #                 "passes_away": stats_away[14],
-        #                 "passes_target_away": stats_away[15],
-        #                 "blocks_away": stats_away[16],
-        #                 "attacks_away": stats_away[17],
-        #                 "dang_attacks_away": stats_away[18]
-        #                 })
         # close match window
         driver.close()
         # go back to window with all matches
@@ -163,6 +118,6 @@ def get_matches(season):
 
 
 # get all data from seasons where we have statistics for matches (from last 111 matches of season 2012/2013)
-for i in range(2018,2023):
+for i in range(2012,2023):
     print(f'{str(i)}-{str(i+1)}')
     get_matches(f'{str(i)}-{str(i+1)}')
